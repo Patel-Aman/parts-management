243df8909edbe24a79866245f58d4e95747570db
Aman Patel <patel.26@iitj.ac.in>
2023-09-03 17:45:31 +0530

    axios config update

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -11,22 +11,16 @@ const apiConfig = axios.create({
 });
 
 // Add an interceptor to include the token in the headers for each request
-apiConfig.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
-        document.cookie = token;
-        // console.log(token);
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
+// apiConfig.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem("token");
+//         document.cookie = token;
+//         // console.log(token);
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
 
 export default apiConfig;
-
-// Function to get the token from local storage
-export function getToken() {
-    const token = localStorage.getItem("token");
-    return token ? JSON.parse(token) : null;
-}
