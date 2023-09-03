845cc4310955eea0f5c41789968a84b6ce432d68
Aman Patel <patel.26@iitj.ac.in>
2023-09-03 20:49:49 +0530

    redirect added logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,9 +18,11 @@ import {
     DropdownItem,
     WindmillContext,
 } from "@windmill/react-ui";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import profileAvatar from "../assets/img/avatar.png";
 
 function Header() {
+    const history = useHistory();
     const { mode, toggleMode } = useContext(WindmillContext);
     const { toggleSidebar } = useContext(SidebarContext);
 
@@ -51,6 +53,7 @@ function Header() {
 
         // Clear localStorage
         localStorage.clear();
+        history.push("/login");
     };
 
     return (
@@ -101,48 +104,6 @@ function Header() {
                             )}
                         </button>
                     </li>
-                    {/* <!-- Notifications menu --> */}
-                    <li className="relative">
-                        <button
-                            className="relative align-middle rounded-md focus:outline-none focus:shadow-outline-purple"
-                            onClick={handleNotificationsClick}
-                            aria-label="Notifications"
-                            aria-haspopup="true"
-                        >
-                            <BellIcon className="w-5 h-5" aria-hidden="true" />
-                            {/* <!-- Notification badge --> */}
-                            <span
-                                aria-hidden="true"
-                                className="absolute top-0 right-0 inline-block w-3 h-3 transform translate-x-1 -translate-y-1 bg-red-600 border-2 border-white rounded-full dark:border-gray-800"
-                            ></span>
-                        </button>
-
-                        <Dropdown
-                            align="right"
-                            isOpen={isNotificationsMenuOpen}
-                            onClose={() => setIsNotificationsMenuOpen(false)}
-                        >
-                            <DropdownItem
-                                tag="a"
-                                href="#"
-                                className="justify-between"
-                            >
-                                <span>Messages</span>
-                                <Badge type="danger">13</Badge>
-                            </DropdownItem>
-                            <DropdownItem
-                                tag="a"
-                                href="#"
-                                className="justify-between"
-                            >
-                                <span>Sales</span>
-                                <Badge type="danger">2</Badge>
-                            </DropdownItem>
-                            <DropdownItem onClick={() => alert("Alerts!")}>
-                                <span>Alerts</span>
-                            </DropdownItem>
-                        </Dropdown>
-                    </li>
                     {/* <!-- Profile menu --> */}
                     <li className="relative">
                         <button
@@ -153,7 +114,7 @@ function Header() {
                         >
                             <Avatar
                                 className="align-middle"
-                                src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82"
+                                src={profileAvatar}
                                 alt=""
                                 aria-hidden="true"
                             />
